8d873b97cff7a34cbb0c97a5ad50b3aadc08574b
Pooja Rathore <poojarathore.nks@gmail.com>
2020-09-14 16:03:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app =express();
 //Connect Database
 connectdDB();
 
-//Init Middleware
+//Initialize Middlewares
 app.use(express.json({ extended: false}));
 
 //app.get('/',(req, res) => res.send('API Running'));
